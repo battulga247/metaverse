1c60a36e786d6642f3b33908d1a7e61f41dd3b08
battulga247 <gersenz.com@gmail.com>
2022-11-28 22:46:02 +0800

    added world and insight

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -1,9 +1,36 @@
 'use client';
+import { motion } from 'framer-motion';
+ 
+import styles from '../styles';
+import { fadeIn} from '../utils/motion';
 
-const InsightCard = () => (
-  <div>
-    Insight Card
-  </div>
+
+
+const InsightCard = ({imgUrl, index, title, subtitle}) => (
+  <motion.div
+    variants={fadeIn('up', 'spring', index*0.5, 1)}
+    className="flex md:flex-row flex-col gap-4"
+  >
+    <img src={imgUrl} alt="" className='md:w-[270px] w-full h-[250px] object-cover rounded-[32px]'/>
+
+    <div className='w-full flex justify-between items-center'>
+      <div className='flex-1 md:ml-[62px] flex flex-col max-w-[650px]'>
+        <h4 className='font-normal lg:text-[42px] text-[26px] text-white'>{title}</h4>
+        <p className='mt-[10px] text-secondary-white font-normal text-[14px] lg:text-[18px]'>{subtitle}</p>
+      </div>
+
+      <div className='lg:flex hidden items items-center justify-center w-[100px] 
+      h-[100px] rounded-full bg-transparent border-[1px] border-white ml-5'>
+        <img 
+          src="arrow.svg"
+          alt=""
+          className='w-[40%] h-[40%] object-contain'
+          />
+
+      </div>
+    </div>
+
+  </motion.div>
 );
 
 export default InsightCard;
--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -1,8 +1,47 @@
 'use client';
+import { motion } from 'framer-motion';
+ 
+import styles from '../styles';
+import { fadeIn, staggerContainer, zoomIn } from '../utils/motion';
+
+import { insights } from '../constants';
+import { InsightCard, TitleText, TypingText } from '../components';
 
 const Feedback = () => (
-  <section>
-    Feedback section
+  <section className={`${styles.paddings} relative z-10`}>
+  <motion.div
+    variants={staggerContainer}
+    initial="hidden"
+    whileInView="show"
+    viewport={{once: 'false', amount: 0.25}}
+    className={`${styles.innerWidth} mx-auto flex flex-col lg:flex-row gap-6`}
+  > 
+      <motion.div
+        variants={fadeIn('right', 'tween', 0.2, 1 )}
+        className="flex-1 lg:max-w-[370px] flex justify-end flex-col gradient-05 
+        sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6a6a6a] relative"
+        >
+          <div className="feedback-gradient p-[30px] z-10">
+              <div >
+                <h4 className='font-bold sm:text-[32px] text-[26px] leading-[36px] sm:leading-[40px]'>
+                  Samantha
+                </h4>
+                <p className='mt-[8px] font-normal sm:text-[18px] text-[12px]
+              sm:leading-[22px] leading-[16px] text-white'>
+                  Founder | Metaversus
+                </p>
+
+              </div>
+              <p className='mt-[24px] font-normal sm:text-[24px] text-[18px]
+              sm:leading-[45px] leading-[39px] text-white'>
+
+
+
+              </p>
+          </div>
+
+      </motion.div>
+    </motion.div>
   </section>
 );
 
