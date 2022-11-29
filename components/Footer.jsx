33bbc473d2842c72dbedf9730fe658e88e312675
battulga247 <gersenz.com@gmail.com>
2022-11-29 16:29:02 +0800

    added footer ended

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -14,19 +14,52 @@ const Footer = () => (
     whileInView="show"
     className={ `${styles.paddings} py-8 relative`}
   >
-      <div className="footer-gradient ">
+      <div className="footer-gradient5 ">
         <div className={`${styles.innerWidth} mx-auto
         flex flex-col gap-8`}>
           <div className="flex items-center justify-between
           flex-wrap gap-5">
             <h4 className="font-bold text-white text-[44px] md:text-[64px]">Enter the Metaverse</h4>
+            <button className="flex items-center h-fit py-4 px-6 bg-[#25618b]
+            rounded-[32px] gap-[12px]">
+                <img src="/headset.svg" alt="" 
+                className="w-[24px] h-[24px] object-contain"/>
+                <span className="font-normal text-[16px] text-white" >ENTER METAVERSE</span>
 
+            </button>
           </div>
+          <div className="flex items-col ">
+            <div className="mb-[50px] h-[2px] bg-white opacity-10 w-full">
+
+              <div className="flex items-center justify-between flex-wrap gap-4 p-5 pt-10">
+                <h4 className="font-extrabold text-[24px] text-white">Metaversus</h4>
+                <p className="font-normal text-[14px] text-white opacity-50">
+                  Copyright &copy 2021-2022 Metaversus. All right reserved.
+                </p>
+
+                <div className="flex gap-4">
+
+                  {socials.map((social) => (
+                    <img 
+                      key={social.name}
+                      src={social.url}
+                      alt={social.name}
+                      className="w-[24px] h-[24px]
+                      object-contain cursor-pointer"
+                    />
+
+                  ))}
+
+                </div>
+
+              </div>
+            </div>
+          </div>
+
 
         </div>
 
       </div>
-    footer
   </motion.footer>
 );
 
